883c522d8a31f88a54d567dfb90285d7469c2415
Alberto Lopez <albertoojeda44@aragon.unam.mx>
2022-04-08 12:02:30 -0500

    cambios

--- src/Controlador/controladorEtiquetas.java
+++ src/Controlador/controladorEtiquetas.java
@@ -17,21 +17,32 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import Modelo.modeloEtiquetas;
+import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
 import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.PdfAnnotation;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfStamper;
 import com.itextpdf.text.pdf.PdfWriter;
+import java.awt.List;
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
+import java.util.Iterator;
+import java.util.Map;
+import javax.imageio.ImageIO;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.PDResources;
 import org.apache.pdfbox.pdmodel.font.PDType1CFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.rendering.PDFRenderer;
 
 /**
  *
@@ -65,7 +76,8 @@ public class controladorEtiquetas implements ActionListener{
                 JOptionPane.showMessageDialog(null, "importación exitosa" + "\n Formato" + archivo.getName().substring(archivo.getName().indexOf(".")));
                 modeloE.importarT(archivo, vistaI.tablapdf);
                 modeloE.addCheckBox(archivo, vistaI.tablapdf);
-                ModificarPdfbox(archivo);
+                ModificarStamper(archivo);
+  //              ModificarPdfbox(archivo);
             } else {
                 JOptionPane.showMessageDialog(null, "agregue la extencion .pdf" + "\n  ejemplo nombre.pdf ");
             }
@@ -82,10 +94,23 @@ public class controladorEtiquetas implements ActionListener{
        if(selecArchivo.showDialog(null, "Crear")==JFileChooser.APPROVE_OPTION){
             archivo2=selecArchivo.getSelectedFile();
          }
-        Document doc = new Document(PageSize.LEGAL_LANDSCAPE);        
-        PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(archivo2));
-      stamper.addAnnotation(PdfAnnotation.createText(stamper.getWriter(), new Rectangle(30f, 750f, 80f, 800f), "inserted page", "This page is the title page.", true, null),
-     reader.getNumberOfPages());
+       Document doc = new Document(PageSize.LEGAL_LANDSCAPE);        
+       PdfWriter pdf = PdfWriter.getInstance(doc, new FileOutputStream(archivo));
+       Paragraph parrafo = new Paragraph();
+        parrafo.setAlignment(Paragraph.ALIGN_CENTER);
+         parrafo.setFont(FontFactory.getFont("Tahoma", 12, Font.BOLD, BaseColor.DARK_GRAY));
+         parrafo.setIndentationLeft(8);
+        parrafo.setIndentationRight(8);
+        doc.open();
+         parrafo.add("hola");
+
+                doc.add(parrafo);
+             
+                           doc.close();
+
+       PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(archivo2));
+       stamper.addAnnotation(PdfAnnotation.createText(stamper.getWriter(), new Rectangle(30f, 750f, 80f, 800f), "inserted page", "This page is the title page.", true, null),
+       reader.getNumberOfPages());
 
         stamper.close();
         reader.close();
@@ -93,29 +118,30 @@ public class controladorEtiquetas implements ActionListener{
        
     }   
     public void ModificarPdfbox( File archivo) throws IOException, DocumentException{    
-        PDDocument doc = PDDocument.load(archivo);
-        PDPage page = doc.getPage(1);
-        PDPageContentStream contentStream = new PDPageContentStream(doc, page);
-        contentStream.beginText();
-        contentStream.newLineAtOffset(100,100);
-        contentStream.setFont(PDType1Font.TIMES_BOLD, 12);
-        String text = "This is the sample document and we are adding content to it.";
-        contentStream.showText(text);      
-contentStream.endText();
+       File newfile=new File(archivo.getPath());
+        
+        PDDocument pdfDocument = PDDocument.load(newfile);
+  
+        // PDFRenderer class to be Instantiated
+        // i.e. creating it's object
+        PDFRenderer pdfRenderer
+            = new PDFRenderer(pdfDocument);
+  
+        // Rendering an image
+        // from the PDF document
+        // using BufferedImage class
+        BufferedImage img = pdfRenderer.renderImage(0);
+        // Writing the extracted
+        // image to a new file
+        ImageIO.write(
+            img, "JPEG",
+            new File("C:/Documents/GeeksforGeeks.png"));
+        System.out.println(
+            "Image has been extracted successfully");
+  
+        // Closing the PDF document
+        pdfDocument.close();
 
-      System.out.println("Content added");
-
-      //Closing the content stream
-      contentStream.close();
-
-      //Saving the document
-      if(selecArchivo.showDialog(null, "Crear")==JFileChooser.APPROVE_OPTION){
-            archivo2=selecArchivo.getSelectedFile();
-         }
-      doc.save(new FileOutputStream(archivo2));
-
-      //Closing the document
-      doc.close();
     }
     
     public void actionPerformed(ActionEvent e) {
